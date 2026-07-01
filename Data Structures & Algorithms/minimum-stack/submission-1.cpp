a9aff470652d54b56f21efb1ed225ca16d282e02
rakshit1610 <prakshitm2@gmail.com>
2026-07-01 17:24:36 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,34 @@
+class MinStack {
+public:
+int size=10000;
+int *arr=new int[size];
+int i=-1;
+    MinStack() {
+           
+    }
+    
+    void push(int val) {
+        i++;
+        arr[i]=val;
+    }
+    
+    void pop() {   
+        if(i==-1){
+            return ;
+        }
+         i--;
+    }
+    
+    int top() {
+        return arr[i];
+        
+    }
+    
+    int getMin() {
+        int mini=INT_MAX;
+        for(int j=0;j<=i;j++){
+           mini=min(mini,arr[j]);
+        }
+        return mini;
+    }
+};
