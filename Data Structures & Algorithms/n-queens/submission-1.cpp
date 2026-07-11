595e34ed3b9f781c45402cbe6a93fb42f0485c2f
rakshit1610 <prakshitm2@gmail.com>
2026-07-11 11:59:46 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+vector<vector<string>>ans;
+bool valid(int row,int col,  vector<string>&visited){
+    int n=visited.size();
+   for(int i=0;i<n;i++){
+      if(visited[row][i]=='Q'){
+        return false;
+      }
+      if(visited[i][col]=='Q'){
+        return false;
+      }
+   }   
+   int a=row;
+   int b=col;
+   while(a>=0&&b>=0){
+    if(visited[a][b]=='Q'){
+        return false;
+    } 
+    a--;
+    b--;
+   }
+   int c=row;
+   int d=col;
+   while(c<n&&d>=0){
+    if(visited[c][d]=='Q'){
+        return false;
+    } 
+    c++;
+    d--;
+   }
+   int e=row;
+   int f=col;
+   while(e>=0&&f<n){
+    if(visited[e][f]=='Q'){
+        return false;
+    } 
+    e--;
+    f++;
+   }
+   int g=row;
+   int h=col;
+   while(g<n&&h<n){
+    if(visited[g][h]=='Q'){
+        return false;
+    } 
+    g++;
+    h++;
+   }
+
+  return true;
+
+}
+void solve(int row, vector<string>&visited,int n){
+
+     if(row==n)
+    {
+        ans.push_back(visited);
+        return;
+    }
+ for(int col=0;col<n;col++){
+            if(valid(row,col,visited)){
+                visited[row][col]='Q';
+                solve(row+1,visited,n);
+                visited[row][col]='.';
+            }
+        }
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>visited(n,string(n,'.'));     
+     solve(0,visited,n);
+       return ans;
+        
+    }
+};
